545f7a78d44c2fc8976d991f4bfc070fc9bdfab8
Emanuele Furina <emanuele.furina@istitutoagnelli.it>
2025-02-07 08:45:34 +0100

    first commit

---
+++ ProteCt++/ProteCt++.cpp
@@ -0,0 +1,36 @@
+// ProteCt++.cpp : Definisce le funzioni per la libreria statica.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <iostream>
+#include "./ProteCt++.h"
+#include <Windows.h>
+// TODO: Esempio di funzione di libreria
+
+bool IsLibraryLoaded() {
+
+	return true;
+}
+
+
+bool IsSimpleDebuggerPresent() {
+
+	if (IsDebuggerPresent())
+	{
+		return true;
+	}
+}
+
+
+bool CheckRemoteDebugger() {
+
+	
+
+
+}
+
+
+
+
+
---
+++ ProteCt++/ProteCt++.h
@@ -0,0 +1,15 @@
+#ifndef PROTECTPP_H
+#define PROTECTPP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	// Funzione per verificare l'importazione della libreria
+	bool IsLibraryLoaded();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PROTECTPP_H
